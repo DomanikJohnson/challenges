0a9818c2453a188d523827ef6a7fe511bfafeff5
SwiftDom <domanik.johnson@icloud.com>
2021-05-29 13:23:03 -0400

    Added first challenge

---
+++ Player-card-challenge/Player-card-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Player-card-challenge
+//
+//  Created by Domanik Johnson on 5/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Player-card-challenge/Player-card-challenge/Player_card_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Player_card_challengeApp.swift
+//  Player-card-challenge
+//
+//  Created by Domanik Johnson on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Player_card_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
