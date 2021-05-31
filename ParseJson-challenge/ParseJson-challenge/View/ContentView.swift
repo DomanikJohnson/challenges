fab8aa6b71dbfd12c5fd5b8baa55e617c8229d84
SwiftDom <domanik.johnson@icloud.com>
2021-05-31 12:55:21 -0400

    Tried same output with scrolllview and foreach '

--- ParseJson-challenge/ParseJson-challenge/View/ContentView.swift
+++ ParseJson-challenge/ParseJson-challenge/View/ContentView.swift
@@ -12,41 +12,70 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            List(model.pizzaOrder) { elm in
+        ScrollView {
+            VStack(alignment: .leading) {
+            ForEach(model.pizzaOrder) { pizza in
                 VStack(alignment: .leading) {
                     HStack(alignment: .top) {
-                        Image(elm.image)
+                        Image(pizza.image)
                             .resizable()
                             .scaledToFill()
                             .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                             .clipped()
                         VStack(alignment: .leading) {
-                            
-                        
-                            Text(elm.name)
-                                .multilineTextAlignment(.trailing)
-                            
+                            Text(pizza.name)
+                              .multilineTextAlignment(.trailing)
                             HStack{
-                                Text(elm.toppings[0])
-                                Text(elm.toppings[1])
-                                Text(elm.toppings[2])
+                            Text(pizza.toppings[0])
+                            Text(pizza.toppings[1])
+                            Text(pizza.toppings[2])
                             }
                             .font(.caption)
                         }
-                           
-                    
                     }
-              
-                 
-                     
-                
+                    
                 }
                 
             }
+            
+        }
+        }
+        
+//        VStack {
+//            List(model.pizzaOrder) { elm in
+//                VStack(alignment: .leading) {
+//                    HStack(alignment: .top) {
+//                        Image(elm.image)
+//                            .resizable()
+//                            .scaledToFill()
+//                            .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                            .clipped()
+//                        VStack(alignment: .leading) {
+//
+//
+//                            Text(elm.name)
+//                                .multilineTextAlignment(.trailing)
+//
+//                            HStack{
+//                                Text(elm.toppings[0])
+//                                Text(elm.toppings[1])
+//                                Text(elm.toppings[2])
+//                            }
+//                            .font(.caption)
+//                        }
+//
+//
+//                    }
+//
+//
+//
+//
+//                }
+//
+//            }
         }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
